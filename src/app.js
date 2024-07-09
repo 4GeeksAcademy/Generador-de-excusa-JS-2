c85a4f8fdd3f4c0e4d5e0549261c0318d6191c9d
Amanda Dias <168840059+mnddevl@users.noreply.github.com>
2024-07-09 22:23:52 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = listElement => {
+    let element = "";
+    element = listElement[Math.floor(Math.random() * listElement.length)];
+    return element;
+  };
+
+  let excuseGen = document.querySelector("#excuse");
+
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
 };
